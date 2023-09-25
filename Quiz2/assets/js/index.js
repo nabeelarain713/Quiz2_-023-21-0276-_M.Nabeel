a8f468a02b3b24bcb8d2a23adc052e799209e58b
nabeelarain713 <nabeelarain713@gmail.com>
2023-09-25 12:27:08 +0500

    Commit1!

---
+++ Quiz2/assets/js/index.js
@@ -0,0 +1,23 @@
+//Task 1: Write a function that can calculate the average of an array of values (numbers)
+
+function average(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    return sum / arr.length;
+}
+
+let arr = [15, 20, 25, 40, 10, 40];
+result = average(arr);
+console.log(result);
+
+//Task 2. Print values of array using map function along with index as following output (”Value at index 1 is 10”, …)
+
+function display(arr2) {
+
+}
+
+let arr2 = 
+
